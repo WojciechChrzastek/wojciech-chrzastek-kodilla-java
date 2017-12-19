36d57ef63e0ddf92febab70486675014c46a0f2c
WojciechChrzastek <w.chrzastek@gmail.com>
2017-12-19 14:58:11 +0100

    Zadanie 7.2: Niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -31,5 +32,9 @@ public class StreamMain {
         System.out.println("Własne upiększacze:");
         poemBeautifier.beautify("aaa", (poem) -> poem.toUpperCase() + poem.toLowerCase() + poem.toUpperCase());
         poemBeautifier.beautify("aaa", (poem) -> ">>> " + poem + " <<<");
+
+        System.out.println("");
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
