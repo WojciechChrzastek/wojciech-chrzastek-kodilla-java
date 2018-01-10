daa354629d9db7be4c62328117d46f53ef4c20c7
WojciechChrzastek <w.chrzastek@gmail.com>
2018-01-10 10:50:30 +0100

    Zadanie 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,46 +3,28 @@ package com.kodilla.testing.collection;
 import java.util.ArrayList;
 import java.util.Random;
 
-class Application {
-    public static void main(String args[]) throws java.lang.Exception {
-
-        RandomArrayOfNumbersGenerator array = new RandomArrayOfNumbersGenerator();
-        ArrayList<Integer> numbers = array.generateRandomArrayOfNumbers();
-        System.out.println("Lista " + RandomArrayOfNumbersGenerator.arraySize + " liczb typu Integer: " + numbers + "\n");
-
-        OddNumbersExterminator array2 = new OddNumbersExterminator();
-        ArrayList<Integer> evenNumbersArrayList = array2.exterminate(numbers);
-        System.out.println("Lista liczb parzystych typu Integer: " + evenNumbersArrayList);
-    }
-}
-
 public class OddNumbersExterminator {
-    static ArrayList<Integer> evenNumbersArrayList = new ArrayList<>();
+    public ArrayList<Integer> evenNumbersArrayList = new ArrayList<>();
+    public int arraySize = 10;
 
-    public static ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
-        for(Integer element : numbers) {
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+        for (Integer element : numbers) {
             if (element % 2 == 0) {
                 evenNumbersArrayList.add(element);
             }
         }
-        //System.out.println(evenNumbersArrayList.size());
         return evenNumbersArrayList;
     }
-}
 
-class RandomArrayOfNumbersGenerator {
-    static ArrayList<Integer> numbers;
-    public static int arraySize = 10;
-    public static ArrayList<Integer> generateRandomArrayOfNumbers() {
-
+    public ArrayList<Integer> generateRandomArrayOfNumbers() {
         Random randomNumber = new Random();
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> numbers = new ArrayList<>();
 
         for (int i = 0; i < arraySize; i++) {
             Integer number = randomNumber.nextInt(10);
             numbers.add(number);
         }
-        //System.out.println(numbers.size());
         return numbers;
     }
 }
+
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,8 +1,5 @@
 package com.kodilla.testing.collection;
 
-import com.kodilla.testing.collection.Application;
-import com.kodilla.testing.collection.OddNumbersExterminator;
-import com.kodilla.testing.collection.RandomArrayOfNumbersGenerator;
 import org.junit.*;
 import java.util.ArrayList;
 
@@ -11,39 +8,49 @@ public class CollectionTestSuite {
     public void before() {
         System.out.println("Test Case: begin");
     }
+
     @After
     public void after() {
         System.out.println("Test Case: end");
     }
+
     @BeforeClass
     public static void beforeClass() {
         System.out.println("Test Suite: begin");
     }
+
     @AfterClass
     public static void afterClass() {
         System.out.println("Test Suite: end");
     }
+
     @Test //czy klasa zachowuje się poprawnie gdy lista jest pusta
     public void testOddNumbersExterminatorEmptyList() {
         //Given
-        OddNumbersExterminator array = new OddNumbersExterminator();
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         //When
-        int result = RandomArrayOfNumbersGenerator.generateRandomArrayOfNumbers().size();
-        System.out.println("Testing empty list... List size: " + result);
-        System.out.println(result + " > 0, so the list is not empty.");
+        int randomArraySize = oddNumbersExterminator.generateRandomArrayOfNumbers().size();
+        System.out.println("Testing empty list... List size: " + randomArraySize + " > 0, so the list is not empty.");
         //Then
-        Assert.assertTrue(result > 0);
+        Assert.assertTrue(randomArraySize > 0);
     }
 
     @Test //czy klasa zachowuje się poprawnie gdy lista zawiera liczby parzyste i nieparzyste
     public void testOddNumbersExterminatorNormalList() {
         //Given
-        OddNumbersExterminator array = new OddNumbersExterminator();
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         //When
-        int result = (RandomArrayOfNumbersGenerator.generateRandomArrayOfNumbers().size()) - ((OddNumbersExterminator.exterminate(RandomArrayOfNumbersGenerator.generateRandomArrayOfNumbers())).size());
-        System.out.println("Testing odd and even... Odd and even list size - even list size = " + result);
-        System.out.println(result + " > 0, so the list has odd and even numbers.");
+        ArrayList<Integer> randomArray = oddNumbersExterminator.generateRandomArrayOfNumbers();
+        ArrayList<Integer> evenArray = oddNumbersExterminator.exterminate(randomArray);
+        int randomArraySize = randomArray.size();
+        int evenArraySize = evenArray.size();
+        int oddArraySize = randomArraySize - evenArraySize;
+
+        System.out.println("Random list: " + randomArray + " size: " + randomArraySize);
+        System.out.println("Even list: " + evenArray + " size: " + evenArraySize);
+        System.out.println("Testing odd and even... Odd and even list size - even list size = odd list size = " + oddArraySize);
+        System.out.println(oddArraySize + " > 0, so the list has odd and even numbers.");
         //Then
-        Assert.assertTrue(result > 0);
+        Assert.assertTrue(oddArraySize > 0);
     }
 }
