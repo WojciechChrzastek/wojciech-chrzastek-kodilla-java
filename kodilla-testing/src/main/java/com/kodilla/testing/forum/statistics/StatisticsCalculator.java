9f7962b52af6965932aa16219a262513820500a9
WojciechChrzastek <w.chrzastek@gmail.com>
2018-01-14 15:48:28 +0100

    Wszystko pliki

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCalculator.java
@@ -0,0 +1,43 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.List;
+/*
+public class StatisticsCalculator {
+    Statistics statistics;
+
+    public StatisticsCalculator(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+
+
+
+
+
+public double calculateAdvStatistics(Statistics statistics) {
+
+
+}
+
+
+    public String ShowStatistics() {
+
+}
+
+
+}
+*/
+
+   /* W pakiecie com.kodilla.testing.forum.statistics utwórz klasę obliczającą następujące statystyki:
+
+        Ilość użytkowników
+        Ilość postów
+        Ilość komentarzy
+        Średnia ilość postów na użytkownika
+        Średnia ilość komentarzy na użytkownika
+        Średnia ilość komentarzy na post
+
+        Stworzona klasa powinna posiadać metodę calculateAdvStatistics(Statistics statistics),
+        która obliczy podane powyżej wartości i zapamięta je we właściwościach (polach) klasy.
+
+        */
