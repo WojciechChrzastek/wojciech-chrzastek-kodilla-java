ee22d13f94354a747c16db00aeffb35df166bfb1
WojciechChrzastek <w.chrzastek@gmail.com>
2018-01-27 19:16:46 +0100

    Zadanie 6.5 Mockujemy forum dyskusyjne // Commit 1

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCalculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCalculator.java
@@ -1,43 +1,53 @@
 package com.kodilla.testing.forum.statistics;
 
-import java.util.List;
-/*
 public class StatisticsCalculator {
-    Statistics statistics;
+    private int userCount;
+    private int postCount;
+    private int commentCount;
+    private double avgPostCountPerUser;
+    private double avgCommentCountPerUser;
+    private double avgCommentCountPerPost;
 
-    public StatisticsCalculator(Statistics statistics) {
-        this.statistics = statistics;
+    public void calculateAdvStatistics(Statistics statistics) {
+        userCount = statistics.usersNames().size();
+        postCount = statistics.postsCount();
+        commentCount = statistics.commentsCount();
+        avgPostCountPerUser = postCount / userCount;
+        avgCommentCountPerUser = commentCount / userCount;
+        avgCommentCountPerPost = commentCount / postCount;
     }
 
+    public void showStatistics() {
+        System.out.println("Statistics:\n"
+                + "User count = " + userCount + "\n"
+                + "Post count = " + postCount + "\n"
+                + "Comment count = " + commentCount + "\n"
+                + "Average post count per user = " + avgPostCountPerUser + "\n"
+                + "Average comment count per user = " + avgCommentCountPerUser + "\n"
+                + "Average comment count per post = " + avgCommentCountPerPost);
+    }
 
+    public int getUserCount() {
+        return userCount;
+    }
 
+    public int getPostCount() {
+        return postCount;
+    }
 
+    public int getCommentCount() {
+        return commentCount;
+    }
 
+    public double getAvgPostCountPerUser() {
+        return avgPostCountPerUser;
+    }
 
-public double calculateAdvStatistics(Statistics statistics) {
+    public double getAvgCommentCountPerUser() {
+        return avgCommentCountPerUser;
+    }
 
-
+    public double getAvgCommentCountPerPost() {
+        return avgCommentCountPerPost;
+    }
 }
-
-
-    public String ShowStatistics() {
-
-}
-
-
-}
-*/
-
-   /* W pakiecie com.kodilla.testing.forum.statistics utwórz klasę obliczającą następujące statystyki:
-
-        Ilość użytkowników
-        Ilość postów
-        Ilość komentarzy
-        Średnia ilość postów na użytkownika
-        Średnia ilość komentarzy na użytkownika
-        Średnia ilość komentarzy na post
-
-        Stworzona klasa powinna posiadać metodę calculateAdvStatistics(Statistics statistics),
-        która obliczy podane powyżej wartości i zapamięta je we właściwościach (polach) klasy.
-
-        */
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -1,4 +1,48 @@
 package com.kodilla.testing.forum.statistics;
 
+import com.github.javafaker.Faker;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class ForumStatisticsTestSuite {
+    @Test
+    public void testCalculateAdvStatistics() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator();
+
+        int usersCount = 100;
+        List<String> usersNamesList = generateListOfNUsersNames(usersCount);
+        int postsCount = 1000;
+        int commentsCount = 10;
+
+        when(statisticsMock.usersNames()).thenReturn(usersNamesList);
+        when(statisticsMock.postsCount()).thenReturn(postsCount);
+        when(statisticsMock.commentsCount()).thenReturn(commentsCount);
+
+        //When
+        statisticsCalculator.calculateAdvStatistics(statisticsMock);
+        statisticsCalculator.showStatistics();
+
+        //Then
+        assertEquals(commentsCount / postsCount, statisticsCalculator.getAvgCommentCountPerPost(), 0);
+        assertEquals(commentsCount / usersCount, statisticsCalculator.getAvgCommentCountPerUser(), 0);
+        assertEquals(postsCount / usersCount, statisticsCalculator.getAvgPostCountPerUser(), 0);
+    }
+
+    private List<String> generateListOfNUsersNames(int usersCount) {
+        Faker faker = new Faker();
+        List<String> usersNamesList = new ArrayList<>();
+        for (int n = 1; n <= usersCount; n++) {
+            String userName = faker.name().firstName();
+            usersNamesList.add(userName);
+        }
+        return usersNamesList;
+    }
 }
