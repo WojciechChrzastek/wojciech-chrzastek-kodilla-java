428415f4f8ddc81093e2ff0db5eeb2e635bc5c75
WojciechChrzastek <w.chrzastek@gmail.com>
2018-03-19 13:38:26 +0100

    Kodilla Module 11 // Refactor the code

--- kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -2,7 +2,7 @@ package com.kodilla.patterns.strategy.social;
 
 import org.junit.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class UserTestSuite {
     @Before
